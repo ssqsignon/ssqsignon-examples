af03129c55d25edcfe6e896773a2b4e95775d715
Piotr Wójcik <piotr.wojcik@rivierasolutions.pl>
2015-08-25 23:03:36 +0200

    Added nodejs-angularjs example.

---
+++ nodejs-angularjs/sso-slave/client/app.js
@@ -0,0 +1,69 @@
+angular.module('tinyUsersExampleSSOSlaveApp', [ 'ui.bootstrap', 'angular-ssqsignon', 'ngResource' ])
+    .config(function(authenticatorProvider, $httpProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        authenticatorProvider.init('cat-and-dog', 29, undefined, 'https://tinyusers.azurewebsites.net');
+        $httpProvider.interceptors.push('appendAccessToken');
+        //$httpProvider.interceptors.push('refreshAccessToken');
+    })
+    .controller('landingCtrl', function($scope, $q, $resource, $http, $modal, $location, authenticator) {
+
+        $scope.logout = function() {
+            $scope.hamster = null;
+            $scope.loggedIn = false;
+            authenticator.forgetMe()
+                .then(init);
+        };
+
+        var hamster = $resource('/hamster', undefined, { get: 'GET' });
+
+        init();
+
+        function init() {
+            return authenticator.whoAmI()
+                .catch(function(err) {
+                    if (err == 'ask-user') {
+                        return loginWithMaster();
+                    } else {
+                        return $q.reject(err);
+                    }
+                })
+                .then(function(me) {
+                    if (me != 'redirecting' || me != 'access-denied') {
+                        $scope.userId = me.userId;
+                        $scope.permissions = me.scope;
+                        $scope.loggedIn = true;
+                        getProtectedResources();
+                    }
+                });
+        }
+
+        function loginWithMaster() {
+            if ($location.search().code && $location.search().state) {
+                return $q(function (resolve, reject) {
+                    $http.post('/swapcode', { code: $location.search().code, state: $location.search().state })
+                        .success(function (access) {
+                            $location.search('code', undefined);
+                            $location.search('state', undefined);
+                            resolve(authenticator.ssoSlave.useTokens(access));
+                        })
+                        .error(function (data, status) {
+                            reject({ data: data, status: status });
+                        });
+                });
+            } else if ($location.search().error) {
+                $location.search('error', undefined);
+                $scope.accessDenied = true;
+                return $q.when('access-denied');
+            } else {
+                var uri = authenticator.ssoSlave.loginWithMaster('http://localhost:9901', 'hamster', 'xyz', 'http://localhost:9902');
+                return $q.when('redirecting');
+            }
+        }
+
+        function getProtectedResources() {
+            hamster.get(function(hamster) {
+                $scope.hamster = hamster.message;
+            });
+        }
+
+    });
---
+++ nodejs-angularjs/sso-slave/index.js
@@ -0,0 +1,78 @@
+var express = require('express'),
+    serveStatic = require('serve-static'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    passport = require('passport'),
+    SsqSignonStrategy = require('passport-ssqsignon').Strategy,
+    https = require('https'),
+    port = 9902,
+    app = express();
+
+passport.use(new SsqSignonStrategy('cat-and-dog', scopeAsObject));
+
+app.use(bodyParser.json());
+
+app.post('/swapcode', function(req, res) {
+    consumeAuthorizationCode(req.body.code, function(err, access) {
+        if (err) {
+            res.status(502).send(err);
+        } else {
+            res.send(access);
+        }
+    });
+});
+
+app.get('/hamster', passport.authenticate('ssqsignon', { session: false }), function (req, res) {
+    if (req.user.scope.hamster) {
+        res.json({ message: 'Hello, I am the hamster!' });
+    } else {
+        res.status(403).send('insufficient_permissions');
+    }
+});
+
+
+app.use('/app.js', serveStatic(path.join(__dirname, 'client', 'app.js')));
+app.use('/hamster.jpg', serveStatic(path.join(__dirname, 'client', 'hamster.jpg')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+function scopeAsObject(scopeStr) {
+    return scopeStr.split(' ').reduce(function(result, s) { result[s] = true; return result; }, {});
+}
+
+function consumeAuthorizationCode(code, done) {
+    var module = 'cat-and-dog',
+        clientId = 29,
+        clientSecret = 'testtest',
+        redirectUri = 'http://localhost:9902',
+        data = JSON.stringify({ grant_type: 'authorization_code', code: code, redirect_uri: redirectUri, client_id: clientId });
+    var req = https.request({
+        method: 'POST',
+        host: 'tinyusers.azurewebsites.net',
+        path: [ '', module, 'auth' ].join('/'),
+        auth: [ clientId, clientSecret ].join(':'),
+        headers: { 'Content-Type': 'application/json', 'Content-Length': data.length }
+    }, function(response) {
+        var data = '';
+        response.on('data', function(chunk) {
+            data += chunk;
+        });
+        response.on('end', function() {
+            var parsed = JSON.parse(data);
+            if (response.statusCode == 200) {
+                done(null, parsed);
+            } else {
+                done({ status: response.statusCode, reason: parsed }, null)
+            }
+        });
+    }).on('error', function(e) {
+        done({ status: null, reason: e });
+    });
+    req.write(data);
+    req.end();
+}
+
+app.listen(port);
+console.log([ 'web app listening on port', port.toString() ].join(' '));
