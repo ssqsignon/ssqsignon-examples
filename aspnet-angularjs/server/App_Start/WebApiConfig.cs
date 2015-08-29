96f0092f2126e60143b4f60a49d56ee460f24ca4
Piotr Wójcik <piotr.wojcik@rivierasolutions.pl>
2015-08-30 00:31:59 +0200

    Bumped version of signon package in aspnet servers.

--- aspnet-angularjs/server/App_Start/WebApiConfig.cs
+++ aspnet-angularjs/server/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
 using Newtonsoft.Json.Serialization;
+using Microsoft.Owin.Security.SSQSignon;
 
 namespace server
 {
@@ -14,7 +15,7 @@ namespace server
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter("SSQSignon"));
+            config.Filters.Add(new HostAuthenticationFilter(SSQSignonDefaults.AuthenticationType));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
