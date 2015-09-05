2ed84ffe3867dd021caa2840710e503be85c841a
Piotr Wójcik <piotr.wojcik@rivierasolutions.pl>
2015-09-05 16:39:08 +0200

    Added example http users endpoint to nodejs-angularjs example.

---
+++ nodejs-angularjs/basic-webapp/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ssqSignon: {
+        moduleName: 'cat-and-dog'
+    }
+};
---
+++ nodejs-angularjs/sso-slave-webapp/client/config.js
@@ -0,0 +1,3 @@
+angular.module('ssqSignonExampleConfig', [])
+    .constant('SSQSIGNON_MODULE_NAME', 'cat-and-dog')
+    .constant('SSQSIGNON_CLIENT_ID', 29);
